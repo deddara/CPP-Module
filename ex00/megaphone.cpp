0cf4c2fb9698fce5aa81aa448c8c0f61bb7585d2
deddara <deddara@student.21-school.ru>
2020-10-13 20:21:49 +0300

    megaphone created

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,3 +1,19 @@
-int main(){
+#include <iostream>
+#include <ctype.h>
+
+int		main(int argc, char **argv){
+	char	c;
+
+	if (argc < 2){
+		std::cout << "WHERE ARE MY ARGUMENTS LEBOWSKI???" << std::endl;
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++){
+		for (int j = 0; argv[i][j]; j++){
+			c = toupper(argv[i][j]);
+			std::cout << c;
+		}
+	}
+	std::cout << std::endl;
 	return (0);
 }
