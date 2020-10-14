ebd4b87d3303c8ddf111c74daabb269356280e2e
deddara <deddara@student.21-school.ru>
2020-10-14 20:42:10 +0300

    time added

--- ex02/Account.class.cpp
+++ ex02/Account.class.cpp
@@ -1,5 +1,7 @@
 #include "Account.class.hpp"
 #include <iostream>
+#include <iostream>
+#include <ctime>
 
 int i = -1;
 int Account::_nbAccounts = 0;
@@ -7,9 +9,36 @@ int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
+std::string convertToString(char* a, int size)
+{
+	int i;
+	std::string s = "";
+	for (i = 0; i < size; i++) {
+		s = s + a[i];
+	}
+	return s;
+}
+
+void 	get_time(void)
+{
+	const int MAXLEN = 80;
+	char s[MAXLEN];
+	std::string str;
+	time_t t = time(0);
+
+	strftime(s, MAXLEN, "%Y%m%d", localtime(&t));
+	std::cout << "[" << s << "] ";
+	strftime(s, MAXLEN, "%T", localtime(&t));
+	str = convertToString(s, strlen(s));
+	str.erase(std::remove(str.begin(), str.end(), ':'), str.end());
+	std::cout << "[" << str << "] ";
+}
+
+
 Account::Account(int initial_deposit) : _amount(initial_deposit){
 
 	i++;
+	get_time();
 	this->_accountIndex = i;
 	this->_nbDeposits = 0;
 	this->_nbWithdrawals = 0;
