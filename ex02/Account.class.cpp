5da4df53101bb350e590b946a6eb2a0a9d0d7f3d
deddara <deddara@student.21-school.ru>
2020-10-14 21:37:06 +0300

    finished

--- ex02/Account.class.cpp
+++ ex02/Account.class.cpp
@@ -36,6 +36,24 @@ void	Account::_displayTimestamp(void)
 
 bool Account::makeWithdrawal(int withdrawal)
 {
+	Account::_displayTimestamp();
+
+	if (withdrawal > this->_amount)
+	{
+		std::cout << "index:" << this->_accountIndex << ";p_amount:" << this->_amount \
+		<< ";withdrawal:refused" << std::endl;
+		return (0);
+	}
+	else
+	{
+		Account::_totalNbWithdrawals++;
+		Account::_totalAmount -= withdrawal;
+		this->_nbWithdrawals++;
+		std::cout << "index:" <<  this->_accountIndex << ";p_amount:" << this->_amount \
+		<< ";withdrawal:" << withdrawal;
+		this->_amount -= withdrawal;
+		std::cout << ";amount:" << this->_amount << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+	}
 	return (1);
 }
 
@@ -83,9 +101,9 @@ Account::Account(int initial_deposit) : _amount(initial_deposit){
 }
 
 Account::~Account(void){
-//	get_time();
-//	std::cout << "index:" << this->_accountIndex << ";amount:" \
-//	<< this->_amount << ";closed" << std::endl;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:" \
+	<< this->_amount << ";closed" << std::endl;
 	return ;
 }
 
