9c9e8d55300db88da6c3548b2626cf3c1a0a625a
deddara <deddara@student.21-school.ru>
2020-10-17 13:35:26 +0300

    got file info

--- day01/ex07/sed.cpp
+++ day01/ex07/sed.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
+#include <sstream>
 
 int 	error(std::string str) {
 	std::cout << str << std::endl;
@@ -11,5 +12,14 @@ int main (int argc, char **argv)
 {
 	if (argc != 4)
 		return (error("Wrong num of arguments:("));
+	std::string file_data;
+	std::stringstream buffer;
+
+	std::ifstream ifs(argv[1]);
+	if (!ifs.is_open())
+		return (error("Wrong file"));
+	buffer << ifs.rdbuf();
+	file_data = buffer.str();
+	std::cout << file_data << std::endl;
 	return (0);
 }
