e62c918a351ad08990be81fa81adb236d4324aa5
deddara <deddara@student.21-school.ru>
2020-10-17 13:50:06 +0300

    sed algo works:

--- day01/ex07/sed.cpp
+++ day01/ex07/sed.cpp
@@ -8,18 +8,29 @@ int 	error(std::string str) {
 	return (1);
 }
 
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+	size_t start_pos = 0;
+	while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	}
+	return str;
+}
+
 int main (int argc, char **argv)
 {
 	if (argc != 4)
 		return (error("Wrong num of arguments:("));
-	std::string file_data;
-	std::stringstream buffer;
 
-	std::ifstream ifs(argv[1]);
+	std::string			file_data;
+	std::stringstream	buffer;
+	std::ifstream		ifs(argv[1]);
+
 	if (!ifs.is_open())
 		return (error("Wrong file"));
-	buffer << ifs.rdbuf();
-	file_data = buffer.str();
+	buffer << ifs.rdbuf(); //reading whole file
+	file_data = buffer.str();	//converting to string
+	file_data = ReplaceAll(file_data, argv[2], argv[3]);
 	std::cout << file_data << std::endl;
 	return (0);
 }
