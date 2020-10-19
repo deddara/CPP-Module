d9ea0eb057c52802e9f81cad3a9b49af065fd750
deddara <deddara@student.21-school.ru>
2020-10-19 23:25:38 +0300

    random func added

--- day03/ex00/FragTrap.cpp
+++ day03/ex00/FragTrap.cpp
@@ -1,13 +1,20 @@
 #include "FragTrap.hpp"
 #include <iostream>
 
+const std::string attacks[5] = {
+		"Fire Element", "vegetable juice", "Shock Element", "ice cream", "shield"
+};
+
+const int attacks_dmg[5] = {100, 5, 3, 99999, 50};
+
 FragTrap::FragTrap( std::string name ) : _name(name), _hp(100), _max_hp(100), _ep(100), \
 _max_ep(100), _lvl(1), _ml_dmg(30), _rng_dmg(20), _armor_red(5)
 {
 	std::cout << "Alaka-ZAM! My name is " << name << "!" << std::endl;
 }
 
-FragTrap::~FragTrap( void ) {std::cout << "Argh arghargh death gurgle gurglegurgle urgh... " << this->_name << " faced death." << std::endl;}
+FragTrap::~FragTrap( void ) {std::cout << "Argh arghargh death gurgle gurglegurgle urgh... " \
+<< this->_name << " faced death." << std::endl;}
 
 void FragTrap::rangedAttack(const std::string &target) {
 	std::cout << "FR4G-TP " << this->_name << " attacks " << target << " at range, causing " << this->_rng_dmg \
@@ -30,7 +37,8 @@ void FragTrap::takeDamage(unsigned int amount) {
 		this->_hp = 0;
 	else
 		this->_hp -= amount;
-	std::cout << this->_name << " got attacked with " << start_amount << " damage power! Shield reduced atatck on " \
+	std::cout << "FR4G-TP " <<  this->_name << " got attacked with " << start_amount << \
+	" damage power! Shield reduced atatck on " \
 	<< start_amount - amount << "! Current hp: " << this->_hp << std::endl;
 }
 
@@ -41,5 +49,20 @@ void FragTrap::beRepaired(unsigned int amount) {
 		this->_hp = this->_max_hp;
 	else
 		this->_hp += amount;
-	std::cout << this->_name << " got healed by " << this->_hp - prev_hp << " health points! Now his health is: " << this->_hp << std::endl;
+	std::cout << "FR4G-TP " << this->_name << " got healed by " << this->_hp - prev_hp \
+	<< " health points! Now his health is: " << this->_hp << std::endl;
+}
+
+void FragTrap::vaulthunter_dot_exe(const std::string &target) {
+	if (this->_ep < 25)
+	{
+		std::cout << "Shit... don't have any energy man..." << std::endl;
+	}
+	else {
+		this->_ep -= 25;
+		int index = (std::rand() % (sizeof(attacks) / sizeof(attacks[0])));
+		std::cout << "FR4G-TP " << this->_name << " attacks " << target << " with " << attacks[index] << " causing " \
+		<< attacks_dmg[index] << " points of damage!" << std::endl;
+	}
+
 }
--- day03/ex00/main.cpp
+++ day03/ex00/main.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
+	std::srand(std::time(0));
 	FragTrap t1("Lokh");
 	FragTrap t2("Fack");
 	t1.rangedAttack("zhopa");
 	t2.takeDamage(1500);
 	t1.meleeAttack("zhopa");
 	t2.beRepaired(200);
+	t2.vaulthunter_dot_exe("HUY");
 	return (0);
 }
