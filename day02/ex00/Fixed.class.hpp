2cb843fc023049dfb8f2dd077360841c43f20a9a
deddara <deddara@student.21-school.ru>
2020-10-19 16:13:14 +0300

    some shit added

---
+++ day02/ex00/Fixed.class.hpp
@@ -0,0 +1,19 @@
+#ifndef FIXED_CLASS_H
+# define FIXED_CLASS_H
+
+class fNum{
+
+private:
+	int 	f_val;
+	static const fr_bits;
+
+public:
+	fNum();
+	fNum(fNum const &cpy);
+	~fNum();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+
+};
+
+#endif
