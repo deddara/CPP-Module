5b0bb8303d7256e49831e2b25e748786909ec28f
deddara <deddara@student.21-school.ru>
2020-10-21 17:49:53 +0300

    character cpp

--- day04/ex01/Character.cpp
+++ day04/ex01/Character.cpp
@@ -1,13 +1,23 @@
 #include "Character.hpp"
 #include <iostream>
 
-Character::Character(const std::string &name) : _name(name), _ap(40) { return; }
+Character::Character(const std::string &name) : _name(name), _ap(40), _wpn(0) { return; }
 
 Character::Character(const Character &cpy) {
 	*this = cpy;
 }
 
 Character::~Character() {return ;}
+
+std::string const & Character::getName() const {
+	return _name;
+}
+
+int			Character::getAp() const { return _ap;}
+
+AWeapon		const & Character::getAWeapon() const {
+	return (*_wpn);
+}
 
 void Character::recoverAP() {
 	_ap += 10;
@@ -16,14 +26,34 @@ void Character::recoverAP() {
 }
 
 void Character::attack(Enemy *target) {
-	try {
+	if (_wpn != 0)
 		std::cout << _name << " attacks " << target->getType() << " with a " << _wpn->getName() << std::endl;
 		target->takeDamage(_wpn->getDamage());
 		_ap -= _wpn->getAPCost();
 		if (target->getHP() <= 0)
 			delete target;
 	}
-	catch {
+	else {
 		return;
+	}
+}
+
+Character & Character::operator=(Character &sec_arg) {
+	_name = sec_arg.getName();
+	_wpn = sec_arg.getAWeapon();
+	_ap = getAp();
+}
+
+void Character::equip(AWeapon *wpn) {
+	_wpn = wpn;
+}
+
+std::ostream Character::operator<<(std::ostream &o, Character const & sev_arg)
+{
+	if (_wpn != 0) {
+		o << _name << " has " << _ap << " and wields a " << _wpn->getName() << std::endl;
+	}
+	else {
+		o << _name << " has " << _ap << " and is unarmed" << std::endl;
 	}
 }
--- day04/ex01/Character.hpp
+++ day04/ex01/Character.hpp
@@ -1,5 +1,6 @@
 #ifndef CHARACTER_H
 # define CHARACTER_H
+# include <ostream>
 # include <string>
 # include "AWeapon.hpp"
 # include "Enemy.hpp"
@@ -19,6 +20,12 @@ public:
 	void equip(AWeapon*);
 	void attack(Enemy*);
 	std::string const & getName() const;
+	int 				getAp() const;
+	AWeapon		const &getAWeapon() const;
+
+	Character &operator=(Character &);
 };
+
+std::ostream &operator<<(std::ostream &o, Character const &);
 
 #endif
