d8f0952d2494ed07cf9c551403c497a3a1e1e1a7
deddara <deddara@student.21-school.ru>
2020-10-21 15:22:49 +0300

    aweapon class added:

---
+++ day04/ex01/AWeapon.cpp
@@ -0,0 +1,21 @@
+#include "AWeapon.hpp"
+
+AWeapon::AWeapon(const std::string &name, int apcost, int damage) : _name(name), _apcost(apcost), _damage(damage) { return ;}
+
+AWeapon::AWeapon(const AWeapon &cpy) {
+	*this = cpy;
+}
+
+AWeapon::~AWeapon() { return; }
+
+std::string AWeapon::getName() const {
+	return (_name);
+}
+
+int 		AWeapon::getAPCost() const {
+	return (_apcost);
+}
+
+int			AWeapon::getDamage() const {
+	return (_damage);
+}
---
+++ day04/ex01/AWeapon.hpp
@@ -0,0 +1,24 @@
+#ifndef AWEAPON_H
+# define AWEAPON_H
+# include <string>
+# include <iostream>
+
+class AWeapon{
+
+private:
+	std::string _name;
+	int 		_apcost;
+	int			_damage;
+
+public:
+	AWeapon(std::string const & name, int apcost, int damage);
+	AWeapon(AWeapon const &);
+	~AWeapon();
+
+	std::string getName() const;
+	int getAPCost() const;
+	int getDamage() const;
+	void attack() const = 0;
+};
+
+#endif
