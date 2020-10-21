e4eb57dbc6e63b120c3067ac5e2ffde88ed7f7db
deddara <deddara@student.21-school.ru>
2020-10-21 20:38:12 +0300

    tactical rdy

--- day04/ex02/TacticalMarine.cpp
+++ day04/ex02/TacticalMarine.cpp
@@ -5,15 +5,29 @@ TacticalMarine::TacticalMarine() {
 	std::cout << "Tactical Marine ready for battle!" << std::endl;
 }
 
+TacticalMarine::~TacticalMarine() {
+	std::cout << "Aaargh..." << std::endl;
+}
+
 TacticalMarine::TacticalMarine(const TacticalMarine &cpy) {
 	*this = cpy;
 }
 
-//TacticalMarine *TacticalMarine::clone() const {
-//	TacticalMarine *mar = new TacticalMarine(*this);
-//	return (mar);
-//}
+TacticalMarine &TacticalMarine::operator=(const TacticalMarine &) { return (*this);}
 
-TacticalMarine::~TacticalMarine() {
-	std::cout << "Aaargh..." << std::endl;
+void TacticalMarine::battleCry() const {
+	std::cout << "For the holy PLOT!" << std::endl;
+}
+
+void TacticalMarine::rangedAttack() const {
+	std::cout << "* attacks with a bolter *" << std::endl;
+}
+
+void TacticalMarine::meleeAttack() const {
+	std::cout << "* attacks with a chainsword *" << std::endl;
+}
+
+TacticalMarine *TacticalMarine::clone() const {
+	TacticalMarine *mar = new TacticalMarine(*this);
+	return (mar);
 }
