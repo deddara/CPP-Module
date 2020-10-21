d2e92f5b463c486f3e95cb72266e66e9f16ca432
deddara <deddara@student.21-school.ru>
2020-10-21 17:11:11 +0300

    rad

---
+++ day04/ex01/RadScorpion.cpp
@@ -0,0 +1,10 @@
+#include "RadScorpion.hpp"
+#include <iostream>
+
+RadScorpion::RadScorpion() : Enemy(80, "RadScorpion") {
+	std::cout << "* click click click *" << std::endl;
+}
+
+RadScorpion::~RadScorpion() {
+	std::cout << "* SPROTCH *" << std::endl;
+}
---
+++ day04/ex01/RadScorpion.hpp
@@ -0,0 +1,11 @@
+#ifndef RADSCORPION_H
+# define RADSCORPION_H
+# include "Enemy.hpp"
+
+class RadScorpion : public Enemy{
+public:
+	RadScorpion();
+	~RadScorpion();
+};
+
+#endif
