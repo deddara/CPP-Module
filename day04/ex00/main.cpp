07a2c11e35f4b071d1324fa0f978ac3239b93713
deddara <deddara@student.21-school.ru>
2020-10-21 14:20:10 +0300

    tests wrote

--- day04/ex00/main.cpp
+++ day04/ex00/main.cpp
@@ -1,6 +1,27 @@
 #include "Sorcerer.hpp"
 #include "Victim.hpp"
 #include "Peon.hpp"
+
+void myTest()
+{
+	std::cout << "VIRTUAL TEST" << std::endl;
+	Sorcerer sorc("Sorc", "The Archmage");
+	Victim		vic2("Victim");
+	Peon 		peon("Peon");
+	Peon		over("Over");
+	Victim		*vic = &peon;
+	sorc.polymorph(*vic);
+	vic = &vic2;
+	sorc.polymorph(*vic);
+	std::cout << "---------------------------------------" << std::endl;
+	std::cout << "OVERLOAD TEST" << std::endl;
+	std::cout << peon << vic2;
+	vic2 = peon;
+	std::cout << vic2;
+	peon = over;
+	std::cout << peon;
+}
+
 int main()
 {
 	Sorcerer robert("Robert", "the Magnificent");
@@ -9,5 +30,7 @@ int main()
 	std::cout << robert << jim << joe;
 	robert.polymorph(jim);
 	robert.polymorph(joe);
+
+//	myTest();
 	return 0;
 }
