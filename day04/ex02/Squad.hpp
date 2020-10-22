d072d0afa6a0be4de950fff3faf54f15489f2500
deddara <deddara@student.21-school.ru>
2020-10-22 11:07:57 +0300

    some hpp added

--- day04/ex02/Squad.hpp
+++ day04/ex02/Squad.hpp
@@ -1,13 +1,32 @@
 #ifndef SQUAD_H
 # define SQUAD_H
 # include "ISquad.hpp"
+# include "ISpaceMarine.hpp"
+
+typedef struct	s_squad
+{
+
+		ISpaceMarine	*unit;
+		struct s_squad	*next;
+
+}				t_squad;
+
 
 class Squad : public ISquad
 {
+private:
+	t_squad *squad;
+	static int count;
+
 public:
-	int getCount() const;
-	ISpaceMarine* getUnit(int) const;
-	int push(ISpaceMarine*);
+	Squad();
+	Squad(Squad const &);
+	virtual ~Squad();
+
+	int				getCount() const;
+	int				push(ISpaceMarine*);
+	ISpaceMarine*	getUnit(int) const;
+
 };
 
 #endif
