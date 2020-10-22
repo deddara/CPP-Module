f2ec9ec3300f9e2e022942c314093bfb96115389
deddara <deddara@student.21-school.ru>
2020-10-22 16:40:09 +0300

    character up

--- day04/ex03/Character.cpp
+++ day04/ex03/Character.cpp
@@ -1,7 +1,26 @@
 #include "Character.hpp"
+#include <string>
 
 Character::Character(std::string name) : _name(name) {
+	int i = 0;
+	while (i < 4)
+	{
+		_materia[i] = nullptr;
+		i++;
+	}
 	return ;
+}
+
+Character::~Character(){
+	int i = 0;
+	while (i < 4)
+	{
+		if (_materia[i] != nullptr) {
+			delete _materia[i];
+			_materia[i] = nullptr;
+		}
+		i++;
+	}
 }
 
 Character::Character(const Character &cp) {
@@ -11,5 +30,19 @@ Character::Character(const Character &cp) {
 Character & Character::operator=(const Character &sec_arg) {
 	if (this == &sec_arg)
 		return (*this);
-
+	this->~Character();
+	int i = 0;
+	while (i < 4)
+	{
+		if (sec_arg._materia[i] != nullptr)
+		{
+			_materia[i] = sec_arg._materia[i]->clone();
+		}
+		i++;
+	}
+	_name = sec_arg.getName();
+	return (*this);
 }
+
+std::string	const & Character::getName() const { return _name;}
+
