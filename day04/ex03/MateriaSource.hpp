51573902f86f07eb3c717d92d2b892b814ef7ed6
deddara <deddara@student.21-school.ru>
2020-10-22 18:13:32 +0300

    header added

---
+++ day04/ex03/MateriaSource.hpp
@@ -0,0 +1,22 @@
+#ifndef MATERIASOURCE_H
+# define MATERIASOURCE_H
+# include "IMateriaSource.hpp"
+# include "AMateria.hpp"
+
+
+class MateriaSource : public IMateriaSource{
+
+private:
+	AMateria *_materia[4];
+
+public:
+	MateriaSource();
+	MateriaSource(MateriaSource const &);
+	MateriaSource & operator=(MateriaSource const &);
+	virtual ~MateriaSource();
+
+	virtual void learnMateria(AMateria *);
+	virtual AMateria* createMateria(std::string consr & type);
+};
+
+#endif
