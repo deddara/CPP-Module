73984fbf0f747fff1349039d92c56325731a787a
deddara <deddara@student.21-school.ru>
2020-10-22 19:15:15 +0300

    finish

--- day04/ex03/main.cpp
+++ day04/ex03/main.cpp
@@ -4,6 +4,35 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include <iostream>
+
+void myTest()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Cure());
+	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("lol");
+	std::cout << tmp << std::endl;
+	tmp = src->createMateria("cure");
+	std::cout << tmp->getType() << std::endl;
+	me->equip(nullptr);
+	me->equip(tmp);
+	me->use(0, *me);
+	std::cout << "------" << std::endl;
+	me->unequip(0);
+	me->use(0, *me);
+	me->use(5, *me);
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	me->use(0, *me);
+	delete src;
+	delete me;
+}
+
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
@@ -21,5 +50,6 @@ int main()
 	delete bob;
 	delete me;
 	delete src;
+	myTest();
 	return 0;
 }
