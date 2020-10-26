0da331e3c5f0d59d5a941b144fb56a5be458ac05
deddara <deddara@student.21-school.ru>
2020-10-26 18:35:54 +0300

    range handled

--- day08/ex01/span.cpp
+++ day08/ex01/span.cpp
@@ -6,3 +6,13 @@ void Span::addNumber(int num) {
 	else
 		_numbers.push_back(num);
 }
+
+void Span::addNumber(std::vector<int>::iterator &it1, std::vector<int>::iterator &it2) {
+	while(it1 != it2)
+	{
+		if (_numbers.size() == _n)
+			throw std::string("Array is already filled");
+		_numbers.push_back(*it1);
+		it1++;
+	}
+}
--- day08/ex01/span.hpp
+++ day08/ex01/span.hpp
@@ -1,15 +1,16 @@
 #ifndef SPAN_H
 # define SPAN_H
 # include <vector>
-
+# include <string>
 class Span{
 private:
-	unsigned int _n;
+	unsigned int const _n;
 	std::vector<int>(_numbers);
 public:
-	Span(unsigned int n) : _n(n);
+	Span(unsigned int n) : _n(n) { };
 
 	void addNumber(int num);
+	void addNumber(std::vector<int>::iterator &it1, std::vector<int>::iterator &it2);
 };
 
 #endif
