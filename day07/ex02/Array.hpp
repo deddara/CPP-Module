c24361ecf55ad71275c574f37c423fc697810b11
deddara <deddara@student.21-school.ru>
2020-10-26 15:03:43 +0300

    finished

---
+++ day07/ex02/Array.hpp
@@ -0,0 +1,49 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+# include <iostream>
+
+template <typename T>
+class Array{
+private:
+	T *_arr;
+	unsigned int _n;
+public:
+	Array() :  _arr(new T[0]), _n(0){ return ; }
+	Array(unsigned int n) : _arr(new T(n)), _n(n) { return; }
+	~Array<T>() {
+		if (_arr)
+			delete [] _arr;
+	};
+
+	Array(Array<T> const &cp){
+		*this = cp;
+	}
+
+	Array & operator=(Array const &sec_arg){
+		if (this == &sec_arg)
+			return (*this);
+		if (_arr)
+			delete [] _arr;
+		_arr = new T[sec_arg._n];
+		int i = 0;
+		while (i < sec_arg._n)
+		{
+			_arr[i] = sec_arg._arr[i];
+			i++;
+		}
+		_n = sec_arg._n;
+		return (*this);
+	}
+
+	T	&operator[](int const i)const
+	{
+		if (i < 0 || i >= this->size())
+			throw std::out_of_range("Out of range");
+		else
+			return (this->_arr[i]);
+	}
+
+	unsigned int  	size(void)const{ return _n; }
+};
+
+#endif
---
+++ day07/ex02/main.cpp
@@ -0,0 +1,103 @@
+#include "Array.hpp"
+
+int main()
+{
+	std::cout << "EMPTY ARR TEST" << std::endl;
+	Array<char> arr;
+	Array<int> arr1;
+	Array<Array<char> > arr2;
+	std::cout << arr.size() << std::endl;
+	std::cout << arr1.size() << std::endl;
+	std::cout << arr2.size() << std::endl;
+	try{
+		arr1[0];
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	try{
+		arr2[0];
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "-----------------" << std::endl;
+	Array<double> doub(10);
+	std::cout << doub.size() << std::endl;
+	try{
+		doub[0] = 1.3;
+		std::cout << doub[0] << std::endl;
+		std::cout << doub[20] << std::endl;
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "-----------------" << std::endl << "ASSIGNATION TEST" << std::endl;
+	Array<int> int_arr(10);
+	std::cout << "int_arr: ";
+	try {
+		for (int i = 0; i < 10; i++) {
+			int_arr[i] = i;
+			std::cout << int_arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "int_arr1: ";
+	Array<int> int_arr1(10);
+	try {
+		for (int i = 0; i < 10; i++) {
+			int_arr[i] = i * 2;
+			std::cout << int_arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	catch (std::exception &ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	std::cout << "int_arr1 = int_arr" << std::endl;
+	int_arr1 = int_arr;
+	std::cout << "int_arr1: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr1[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "int_arr: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "int_arr[0] = 100" << std::endl;
+	int_arr[0] = 100;
+	std::cout << "int_arr1: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr1[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "int_arr: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "COPY TEST\n";
+	Array<int> int_arr3(int_arr);
+	std::cout << "int_arr3: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr3[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "int_arr3[0] = 500: ";
+	int_arr3[0] = 500;
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr3[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "int_arr: ";
+	for (int i = 0; i < 10; i++) {
+		std::cout << int_arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+}
