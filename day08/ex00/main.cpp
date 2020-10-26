c59e4f1f09a8d395d4740a6482c73b2de37d5338
deddara <deddara@student.21-school.ru>
2020-10-26 18:00:56 +0300

    tests added

--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -1,16 +1,43 @@
 #include "easyfind.hpp"
+#include <list>
 
 int main()
 {
 	std::vector<int> num;
-	std::vector<int>::iterator it;
+	std::list<int> list;
 	num.push_back(10);
 	num.push_back(11);
 	num.push_back(32);
 	num.push_back(1);
+	list.push_back(40);
+	list.push_back(30);
+	list.push_back(19);
 	try{
-		it = easyfind(num, 10);
-		std::cout << *it << std::endl;
+		std::cout << *easyfind(num, 10) << std::endl;
+	}
+	catch (std::string &ex) {
+		std::cout << ex << std::endl;
+	}
+	try{
+		std::cout << *easyfind(num, -10) << std::endl;
+	}
+	catch (std::string &ex) {
+		std::cout << ex << std::endl;
+	}
+	try{
+		std::cout << *easyfind(num, 11) << std::endl;
+	}
+	catch (std::string &ex) {
+		std::cout << ex << std::endl;
+	}
+	try{
+		std::cout << *easyfind(list, 11) << std::endl;
+	}
+	catch (std::string &ex) {
+		std::cout << ex << std::endl;
+	}
+	try{
+		std::cout << *easyfind(list, 19) << std::endl;
 	}
 	catch (std::string &ex) {
 		std::cout << ex << std::endl;
