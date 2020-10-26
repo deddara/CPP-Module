0ba5e42beb0eddb5806386240b03e8828f8dd739
deddara <deddara@student.21-school.ru>
2020-10-26 11:52:16 +0300

    max min added

--- day07/ex00/whatever.cpp
+++ day07/ex00/whatever.cpp
@@ -9,12 +9,30 @@ void swap(T &val1, T & val2)
 	val2 = temp;
 }
 
-
-int main(void)
+template<typename T>
+T const &max(T const &val1, T const &val2)
 {
+	return (val2 >= val1 ? val2 : val1);
+}
+
+template<typename T>
+T const &min(T const &val1, T const &val2)
+{
+	return (val2 <= val1 ? val2 : val1);
+}
+
+int main( void ) {
 	int a = 2;
 	int b = 3;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	return (0);
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
